371c493b0d18d5b715df050382bd09b94e276a09
nishantkumarr <nishanttdevops@gmail.com>
2025-07-13 16:28:34 +0000

    jenkins-update

--- terraform-aws-jenkins/main.tf
+++ terraform-aws-jenkins/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "my_sg" {
-  name        = "jenkins-security-group"
+  name        = "nishant-security-group"
   description = "Allow SSH and HTTP"
 
   ingress {
